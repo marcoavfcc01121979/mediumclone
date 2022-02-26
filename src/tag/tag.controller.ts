3972f7dc02f25527e9eab1daa37646b17e18f0ab
marco ferreira <marco@smartinnovation.com.br>
2022-02-26 14:15:40 -0300

    initial mediumclone

---
+++ src/tag/tag.controller.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { TagService } from './tag.service';
+import { Controller, Get } from '@nestjs/common';
+import { TagEntity } from './entity/tag.entity';
+
+@Controller('tags')
+export class TagController {
+  constructor(private readonly tagService: TagService) {}
+
+  /*@Get()
+  async findAll(): Promise<TagEntity[]> {
+    return await this.tagService.findAll();
+  }*/
+
+  @Get()
+  async findAll(): Promise<{ tags: string[] }> {
+    const tags = await this.tagService.findAll();
+
+    return {
+      tags: tags.map((tag) => tag.nome)
+    }
+  }
+}
